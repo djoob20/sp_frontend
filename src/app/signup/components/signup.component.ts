90b82003149173297e638f7470e007e23b1d6652
djoob20 <djoob20@gmail.com>
2023-11-24 00:03:13 +0100

    added validators

--- src/app/signup/components/signup.component.ts
+++ src/app/signup/components/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
@@ -13,6 +13,11 @@ export class SignupComponent {
 
   emailRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+  invalidControlName!: string;
+  invalidEmail!: boolean;
+  otherFieldInvalid!: boolean;
+  invalidPassword!: boolean;
+
   constructor(private router:Router, private formBuilder: FormBuilder){
     this.signupForm = this.formBuilder.group({
       firstname: [null, Validators.required],
@@ -21,9 +26,8 @@ export class SignupComponent {
       psw: [null, Validators.required],
       psw_repeat: [null, Validators.required],
     },
-    {
-      updateOn:"submit"
-    })
+      { validators: [this.confirmPasswordValidator] }
+    )
 
     this.submitted = false;
   }
@@ -33,12 +37,67 @@ export class SignupComponent {
   }
 
   onSignup() {
-    console.log("invalide forme: " + this.signupForm.invalid)
+    console.log("invalide form: " + this.signupForm.invalid)
    if(this.signupForm.invalid){
     this.submitted = true;
      document.body.scrollTop = 0;
      document.documentElement.scrollTop = 0;
+     console.log("Invalid controls: " + this.findInvalidControls())
    }
   }
+
+  public findInvalidControls() {
+    const controls = this.signupForm.controls;
+    for (const name in controls) {
+      if (controls[name].invalid) {
+        switch (name) {
+          case "firstname":
+            this.invalidControlName = "prénom";
+            this.invalidEmail = false;
+            this.invalidPassword = false;
+            this.otherFieldInvalid = true;
+            break;
+          case "lastname":
+            this.invalidControlName = "nom";
+            this.invalidEmail = false;
+            this.invalidPassword = false;
+            this.otherFieldInvalid = true;
+            break;
+          case "email":
+            this.otherFieldInvalid = false;
+            this.invalidPassword = false;
+            this.invalidEmail = true;
+            break;
+          case "psw":
+            this.invalidEmail = false;
+            this.otherFieldInvalid = false;
+            this.invalidPassword = true;
+            break;
+          case "psw_repeat":
+            this.invalidEmail = false;
+            this.otherFieldInvalid = false;
+            this.invalidPassword = true;
+            break;
+          default:
+            break;
+        }
+
+
+        return name
+      }
+    }
+    return null;
+  }
+
+  confirmPasswordValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    if(control.value.psw === control.value.psw_repeat){
+      this.invalidPassword = false;
+      return null;
+    }else{
+      this.invalidPassword = true;
+      return { PasswordNoMatch: true };
+    }
+
+  };
 
 }
